0e37f9294201f0a5b03fb5ac3239e56f7ea587c8
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-08-25 21:41:30 -0300

    feat: testando classes

--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -1,8 +1,11 @@
 package app;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Olá Mundo!!");
-    }
+  public static void main(String[] args) {
+    Genero genero = new Genero(1, "ficção");
+    System.out.println(genero.getName());
+    Genero genero2 = new Genero(2, "");
+    System.out.println(genero.getName());
+    System.out.println(genero2.getId());
+  }
 }
-
